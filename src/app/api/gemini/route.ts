1398f7d18a1642bf2184dfc38d61a2e87225428e
nipapin <papin201212@gmail.com>
2026-02-18 16:20:26 +0200

    Update version to 1.2.1 and enhance image export process in useTemplateExport hook by optimizing performance and refining DOM management for better efficiency.

---
+++ src/app/api/gemini/route.ts
@@ -0,0 +1,57 @@
+import { GoogleGenAI } from "@google/genai";
+import { NextResponse } from "next/server";
+import sharp from "sharp";
+
+const genai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
+
+export async function POST(request: Request) {
+    const formData = await request.formData();
+    const image = formData.get("image") as string;
+    const text = formData.get("text") as string;
+    const imageBuffer = Buffer.from(image, "base64");
+    const imageMeta = await sharp(imageBuffer).metadata();
+    const mimeType = `image/${imageMeta.format}`;
+    const originalSize = [imageMeta.width, imageMeta.height];
+
+    const prompt = [
+        { text: text },
+        {
+            inlineData: {
+                mimeType: mimeType,
+                data: image,
+            },
+        },
+    ];
+
+    const response = await genai.models
+        .generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+            config: {
+                responseModalities: ["IMAGE"],
+                candidateCount: 1,
+            },
+        })
+        .then((response) => {
+            return { parts: response.candidates?.[0]?.content?.parts || [] };
+        })
+        .catch((error: any) => {
+            return { error: error.message };
+        });
+
+    if ("error" in response) {
+        return NextResponse.json({ error: response.error }, { status: 500 });
+    }
+
+    for (const part of response.parts) {
+        if (part.inlineData) {
+            const imageData = part.inlineData.data as string;
+            const imageBuffer = Buffer.from(imageData, "base64");
+            await sharp(imageBuffer).resize(originalSize[0], originalSize[1]).toBuffer();
+            return NextResponse.json({ image: imageBuffer.toString("base64") }, { status: 200 });
+        }
+    }
+    return NextResponse.json({ error: "Failed to generate image" }, { status: 500 });
+}
